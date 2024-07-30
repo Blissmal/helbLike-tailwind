2373ed697f6be13904a926d2a0872a261a00cb99
Blissmal <malutibethuel@gmail.com>
2024-07-30 15:33:33 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoad(false);
-    }, 3000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
