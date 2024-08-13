bb61244ac407031ef69e6a9609cfc1f8b37b60f6
Blissmal <malutibethuel@gmail.com>
2024-08-13 11:21:01 +0300

    Updated footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className='w-full bg-white grid sm:flex justify-center items-center gap-20 py-10'>
-      <h2>Copyright © 2024 <a className="text-blue-500" target="_blank" href="https://blissmal-portfolio.vercel.app">Blissmal</a> , All rights reserved.</h2>
+      <h2>Copyright &copy; 2024 <a className="text-blue-500" target="_blank" href="https://blissmal-portfolio.vercel.app">Blissmal</a> , All rights reserved.</h2>
       <h2>Powered By: _de_Bliss_</h2>
     </div>
   )
